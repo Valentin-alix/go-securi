e67bed67a404e1170dcbcaca6e1526cc2ad50473
Samuel Challe <samuel.challe@gmail.com>
2021-12-20 22:17:28 +0100

    add Guard.java

---
+++ src/main/java/com/formation/epsi/gosecuri/model/Guard.java
@@ -0,0 +1,25 @@
+package com.formation.epsi.gosecuri.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Guard {
+
+    private String id;
+    private String firstname;
+    private String lastname;
+    private String fullname;
+    private String job;
+    private String password;
+    private List<Material> materials;
+
+    public String toString() {
+        return "";
+    }
+}
