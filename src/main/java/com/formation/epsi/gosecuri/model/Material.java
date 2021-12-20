1db85df8c8521242f39db95d85cd41d8611e692b
Samuel Challe <samuel.challe@gmail.com>
2021-12-20 22:17:42 +0100

    add Material.java

---
+++ src/main/java/com/formation/epsi/gosecuri/model/Material.java
@@ -0,0 +1,18 @@
+package com.formation.epsi.gosecuri.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Material {
+
+    private String id;
+    private String name;
+
+    public String toString() {
+        return "";
+    }
+}
