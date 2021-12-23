ae60829a0a3c3349705fabb49bf243ac4665abfe
Samuel Challe <samuel.challe@gmail.com>
2021-12-23 22:29:56 +0100

    update Staff.java

--- src/main/java/com/formation/epsi/gosecuri/model/Staff.java
+++ src/main/java/com/formation/epsi/gosecuri/model/Staff.java
@@ -1,29 +1,15 @@
 package com.formation.epsi.gosecuri.model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
-import java.util.Scanner;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class Staff {
     List<Guard> guards = new ArrayList<>();
-
-    public Staff(String dataDir, String dataFile) throws FileNotFoundException {
-        try (Scanner scan = new Scanner(new File(dataDir+dataFile))) {
-            scan.useLocale(Locale.FRANCE);
-            while (scan.hasNext()) {
-                Guard guard = new Guard();
-                String line = scan.nextLine().trim();
-                guard.setId(line);
-                this.guards.add(guard);
-            }
-        }
-    }
 }
