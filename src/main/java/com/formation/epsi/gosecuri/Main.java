4d38e27a8ac3452a0b739f575fc9e6bd5599edd4
Samuel Challe <samuel.challe@gmail.com>
2021-12-23 22:29:38 +0100

    update Main.java

--- src/main/java/com/formation/epsi/gosecuri/Main.java
+++ src/main/java/com/formation/epsi/gosecuri/Main.java
@@ -4,25 +4,34 @@ import com.formation.epsi.gosecuri.model.Guard;
 import com.formation.epsi.gosecuri.model.Staff;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
 
 import freemarker.template.*;
 
 public class Main {
 
+	/* Templates */
 	static String TEMPLATES_PATH = "E:/www/epsi/mspr_tpre500/go-securi-appli/src/main/java/com/formation/epsi/gosecuri/templates";
+	static String TEMPLATE_INDEX = "index.ftl";
+	static String TEMPLATE_GUARD = "guard.ftl";
+
+	/* Data */
 	static String DATA_DIR = "E:/www/epsi/mspr_tpre500/go-securi-data/";
 	static String DATA_FILE = "staff.txt";
+
+	/* Target */
 	static String TARGET_DIR = "out/";
+	static String TARGET_INDEX = "index.html";
 
 	public static void main(String[] args) throws IOException, TemplateException {
 		/* Create freemarker configuration */
 		Configuration cfg = configuration(TEMPLATES_PATH);
 
-		/* Create a new staff */
-		Staff staff = new Staff(DATA_DIR, DATA_FILE);
+		/* Create the guards */
+		List<Guard> guards = guardsFactory();
+
+		/* Create the staff */
+		Staff staff = staffFactory(guards);
 
 		/* Create the homepage */
 		generateIndex(cfg, staff);
@@ -31,11 +40,75 @@ public class Main {
 		generateGuardsPage(cfg, staff);
 	}
 
+	private static List<Guard> guardsFactory() throws IOException {
+		/* Create a list of guards */
+		List<Guard> guards = new ArrayList<>();
+
+		/* Scanning staff txt file */
+		String staffData = DATA_DIR+DATA_FILE;
+		try (Scanner staffDataScan = new Scanner(new File(staffData))) {
+			while (staffDataScan.hasNext()) {
+
+				/* The shortname of the guard is retrieved */
+				String shortname = staffDataScan.nextLine().trim();
+
+				/* A filename is generated */
+				String guardData = DATA_DIR+shortname+".txt";
+				File guardDataFile = new File(guardData);
+
+				/* Scanning txt file of each guard */
+				/* If the file exist */
+				if(guardDataFile.exists()) {
+					/* Declaration of the number of lines counter */
+					int i = 0;
+					Scanner guardDataScan = new Scanner(guardDataFile);
+
+					/* Create a new guard */
+					Guard guard = new Guard();
+					guard.setId(shortname);
+					while (guardDataScan.hasNext()) {
+						/* Counter increment */
+						i++;
+						/* the data is retrieved */
+						String data = guardDataScan.nextLine().trim();
+						if (i == 1) {
+							guard.setLastname(data);
+						} else if (i == 2) {
+							guard.setFirstname(data);
+						} else if (i == 3) {
+							guard.setJob(data);
+						} else if (i == 4) {
+							guard.setPassword(data);
+						}
+					}
+					guardDataScan.close();
+
+					/*  Adding the new guard to the list */
+					guards.add(guard);
+				}
+			}
+		}
+		/*  Return the list */
+		return guards;
+	}
+
+	/**
+	 *
+	 * @param guards List of guards
+	 * @return Staff
+	 */
+	private static Staff staffFactory(List<Guard> guards) {
+		Staff staff = new Staff();
+		staff.setGuards(guards);
+
+		return staff;
+	}
+
 	private static void generateIndex(Configuration cfg, Staff staff) throws TemplateException, IOException {
 		Map<String, Object> indexData = new HashMap<>();
 		indexData.put("title", "Liste des agents");
 		indexData.put("guards", staff.getGuards());
-		generateHTML(cfg, indexData, "index.ftl", "index.html" );
+		generateHTML(cfg, indexData, TEMPLATE_INDEX, TARGET_INDEX);
 	}
 
 	private static void generateGuardsPage(Configuration cfg, Staff staff) throws TemplateException, IOException {
@@ -43,7 +116,7 @@ public class Main {
 			Map<String, Object> guardData = new HashMap<>();
 			guardData.put("title", guard.getId());
 			guardData.put("guard", guard);
-			generateHTML(cfg, guardData, "guard.ftl", String.format("%s.html", guard.getId()) );
+			generateHTML(cfg, guardData, TEMPLATE_GUARD, String.format("%s.html", guard.getId()) );
 		}
 	}
 
