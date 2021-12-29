5e50dc0d3ccbb36a461fd2f04ac340126215a241
Samuel Challe <samuel.challe@gmail.com>
2021-12-29 18:31:22 +0100

    update HtmlFactory.java

--- src/main/java/com/formation/epsi/gosecuri/factory/HtmlFactory.java
+++ src/main/java/com/formation/epsi/gosecuri/factory/HtmlFactory.java
@@ -7,20 +7,25 @@ import freemarker.template.TemplateException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.ResourceBundle;
 
 public class HtmlFactory {
 
-    /* Data */
-    static String DATA_DIR = "E:/www/epsi/mspr_tpre500/go-securi-data/";
+    private static final ResourceBundle resource = ResourceBundle.getBundle("info");
 
     public static boolean create(Configuration cfg, Object templateData, String templateName, String targetFileName) throws IOException, TemplateException {
+        // Get data from properties file
+        String targetDir = resource.getString("target.dir");
+
         /* Get the template (uses cache internally) */
         Template template = cfg.getTemplate(templateName);
 
         /* Merge data-model with template in a new file */
-        try (Writer fileWriter = new FileWriter(DATA_DIR + targetFileName)) {
+        try (Writer fileWriter = new FileWriter(targetDir + targetFileName)) {
             template.process(templateData, fileWriter);
             return true;
+        } catch (IOException e) {
+            return false;
         }
     }
 }
