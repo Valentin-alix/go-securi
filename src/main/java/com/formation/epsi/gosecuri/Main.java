8f7ef03bb93d887e8f97985c17ecc2c7fa4985ce
Samuel Challe <samuel.challe@gmail.com>
2021-12-30 22:16:41 +0100

    update Main.java

--- src/main/java/com/formation/epsi/gosecuri/Main.java
+++ src/main/java/com/formation/epsi/gosecuri/Main.java
@@ -1,7 +1,9 @@
 package com.formation.epsi.gosecuri;
 
 import com.formation.epsi.gosecuri.factory.*;
+import com.formation.epsi.gosecuri.model.Equipment;
 import com.formation.epsi.gosecuri.model.Guard;
+import com.formation.epsi.gosecuri.model.Material;
 import com.formation.epsi.gosecuri.model.Staff;
 
 import java.io.*;
@@ -12,8 +14,14 @@ import freemarker.template.*;
 public class Main {
 
 	public static void main(String[] args) throws IOException, TemplateException {
+		/* Create the equipments */
+		List<Equipment> equipments = EquipmentsFactory.create();
+
+		/* Create the material */
+		Material material = MaterialFactory.create(equipments);
+
 		/* Create the guards */
-		List<Guard> guards = GuardsFactory.create();
+		List<Guard> guards = GuardsFactory.create(material);
 
 		/* Create the staff */
 		Staff staff = StaffFactory.create(guards);
