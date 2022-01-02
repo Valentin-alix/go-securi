ea005b969e869a58987e9ad20cefbd9870a3a5de
Samuel Challe <samuel.challe@gmail.com>
2022-01-02 12:38:56 +0100

    update Main.java

--- src/main/java/com/formation/epsi/gosecuri/Main.java
+++ src/main/java/com/formation/epsi/gosecuri/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
 	public static void main(String[] args) throws IOException, TemplateException {
 		/* Create the equipments */
-		List<Equipment> equipments = EquipmentsFactory.create();
+		HashMap<String, Equipment> equipments = EquipmentsFactory.create();
 
 		/* Create the guards */
 		List<Guard> guards = GuardsFactory.create(equipments);
