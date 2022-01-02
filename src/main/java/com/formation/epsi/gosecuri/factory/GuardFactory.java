9e789bb22987b7a10c8986670e1567d9ab6bce18
Samuel Challe <samuel.challe@gmail.com>
2022-01-02 12:39:14 +0100

    update GuardFactory.java

--- src/main/java/com/formation/epsi/gosecuri/factory/GuardFactory.java
+++ src/main/java/com/formation/epsi/gosecuri/factory/GuardFactory.java
@@ -1,6 +1,10 @@
 package com.formation.epsi.gosecuri.factory;
 
+import com.formation.epsi.gosecuri.model.Equipment;
 import com.formation.epsi.gosecuri.model.Guard;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GuardFactory {
 
@@ -13,13 +17,14 @@ public class GuardFactory {
      * @param password password
      * @return Guard
      */
-    public static Guard create(String shortname, String lastname, String firstname, String job, String password) {
+    public static Guard create(String shortname, String lastname, String firstname, String job, String password, List<Equipment> equipments) {
         Guard guard = new Guard();
         guard.setId(shortname);
         guard.setLastname(lastname);
         guard.setFirstname(firstname);
         guard.setJob(job);
         guard.setPassword(password);
+        guard.setEquipments(equipments);
 
         return guard;
     }
