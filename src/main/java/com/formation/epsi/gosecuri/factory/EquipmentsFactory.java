6f7c864aa13c9c8310b1e067967734bba226db33
Samuel Challe <samuel.challe@gmail.com>
2022-01-02 12:39:31 +0100

    update EquipmentsFactory.java

--- src/main/java/com/formation/epsi/gosecuri/factory/EquipmentsFactory.java
+++ src/main/java/com/formation/epsi/gosecuri/factory/EquipmentsFactory.java
@@ -4,22 +4,19 @@ import com.formation.epsi.gosecuri.model.Equipment;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
-import java.util.Scanner;
+import java.util.*;
 
 public class EquipmentsFactory {
 
     private static final ResourceBundle resource = ResourceBundle.getBundle("info");
 
-    public static List<Equipment> create() throws IOException {
+    public static HashMap<String, Equipment> create() throws IOException {
         // Get data from properties file
         String dataDir = resource.getString("data.dir");
         String dataFileMaterial = resource.getString("data.file.material");
 
-        /* Create a list of equipments */
-        List<Equipment> equipments = new ArrayList<>();
+        /* Create a hashmap of equipments */
+        HashMap<String, Equipment> equipments = new HashMap<>();
 
         /* Scanning liste txt file */
         String materialData = dataDir+dataFileMaterial;
@@ -35,8 +32,8 @@ public class EquipmentsFactory {
                 /* Create a new equipment */
                 Equipment equipment = EquipmentFactory.create(id, name);
 
-                /*  Adding the new equipment to the list */
-                equipments.add(equipment);
+                /*  Adding the new equipment to the hashmap */
+                equipments.put(id, equipment);
             }
         }
 
