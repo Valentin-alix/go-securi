b51bf2aa1db7882ef2070dc10b7b153e648a777b
Samuel Challe <samuel.challe@gmail.com>
2022-01-05 23:18:50 +0100

    update IndexPageFactory.java

--- src/main/java/com/formation/epsi/gosecuri/factory/IndexPageFactory.java
+++ src/main/java/com/formation/epsi/gosecuri/factory/IndexPageFactory.java
@@ -9,25 +9,50 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
 
-public class IndexPageFactory {
+public class IndexPageFactory implements Runnable {
 
-    private static final ResourceBundle resource = ResourceBundle.getBundle("info");
+    Thread thread;
+    private final String name;
+    private final Configuration cfg;
+    private final Staff staff;
+    private final ResourceBundle resource = ResourceBundle.getBundle("info");
 
     /**
      *
      * @param cfg Configuration
      * @param staff Staff
-     * @return boolean
      */
-    public static boolean create(Configuration cfg, Staff staff) throws TemplateException, IOException {
-        // Get data from properties file
-        String templateIndex = resource.getString("template.index");
-        String targetIndex = resource.getString("target.index");
+    public IndexPageFactory(Configuration cfg, Staff staff) {
+        this.cfg = cfg;
+        this.staff = staff;
+        this.name = "index";
+    }
 
-        Map<String, Object> indexData = new HashMap<>();
-        indexData.put("title", "Liste des agents");
-        indexData.put("guards", staff.getGuards());
+    @Override
+    public void run() {
+        try {
+            /* Get data from properties file */
+            String templateIndex = resource.getString("template.index");
+            String targetIndex = resource.getString("target.index");
 
-        return HtmlFactory.create(cfg, indexData, templateIndex, targetIndex);
+            /* Create the indexData hash */
+            Map<String, Object> indexData = new HashMap<>();
+            /* Put string "title" into the indexData */
+            indexData.put("title", "Liste des agents");
+            /* Put guards into the indexData */
+            indexData.put("guards", staff.getGuards());
+
+            /* Create index page */
+            HtmlFactory.create(cfg, indexData, templateIndex, targetIndex);
+        } catch (IOException | TemplateException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start() {
+        if (thread == null) {
+            thread = new Thread(this, name);
+            thread.start();
+        }
     }
 }
