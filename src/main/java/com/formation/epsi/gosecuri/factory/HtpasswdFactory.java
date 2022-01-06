898315e8773a2d209aa1f49a1ef700f7e90fd770
Samuel Challe <samuel.challe@gmail.com>
2022-01-06 18:07:44 +0100

    update HtpasswdFactory.java

--- src/main/java/com/formation/epsi/gosecuri/factory/HtpasswdFactory.java
+++ src/main/java/com/formation/epsi/gosecuri/factory/HtpasswdFactory.java
@@ -1,33 +1,54 @@
 package com.formation.epsi.gosecuri.factory;
 
 import com.formation.epsi.gosecuri.model.Guard;
-import com.formation.epsi.gosecuri.model.Staff;
 
 import java.io.FileWriter;
 import java.io.IOException;
+
+import java.util.List;
 import java.util.ResourceBundle;
 
-public class HtpasswdFactory {
+public class HtpasswdFactory implements Runnable {
 
+    Thread thread;
+    private final String threadName;
+    private final List<Guard> guards;
     private static final ResourceBundle resource = ResourceBundle.getBundle("info");
 
-    public static boolean create(Staff staff) {
-        /* Get data from properties file */
-        String targetDir = resource.getString("target.dir");
+    /**
+     *
+     * @param guards List of guards
+     */
+    public HtpasswdFactory(List<Guard> guards) {
+        this.guards = guards;
+        this.threadName = "htpasswd";
+    }
 
-        /* Create a File */
+    @Override
+    public void run() {
         try {
-            FileWriter myWriter = new FileWriter(targetDir+".htpasswd");
-            for (Guard guard : staff.getGuards()) {
+            /* Get data from properties file */
+            String targetDir = resource.getString("target.dir");
+            
+            /* Create a htpasswd file */
+            FileWriter myWriter = new FileWriter(targetDir + ".htpasswd");
+
+            /* Add guards in htpasswd file */
+            for (Guard guard : guards) {
                 String string = String.format("%s:%s\r\n", guard.getId(), guard.getPassword());
                 myWriter.write(string);
             }
+
             myWriter.close();
-            return true;
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        return false;
+    public void start() {
+        if (thread == null) {
+            thread = new Thread(this, threadName);
+            thread.start();
+        }
     }
 }
