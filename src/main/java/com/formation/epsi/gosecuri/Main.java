79b059a3fd78563e30d2bbebc8f66bba872c7a80
Samuel Challe <samuel.challe@gmail.com>
2022-01-06 18:06:02 +0100

    update Main.java

--- src/main/java/com/formation/epsi/gosecuri/Main.java
+++ src/main/java/com/formation/epsi/gosecuri/Main.java
@@ -3,7 +3,6 @@ package com.formation.epsi.gosecuri;
 import com.formation.epsi.gosecuri.factory.*;
 import com.formation.epsi.gosecuri.model.Equipment;
 import com.formation.epsi.gosecuri.model.Guard;
-import com.formation.epsi.gosecuri.model.Staff;
 
 import java.io.*;
 import java.util.*;
@@ -12,28 +11,29 @@ import freemarker.template.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException, TemplateException {
+	public static void main(String[] args) throws IOException {
 		/* Create the equipments */
 		HashMap<String, Equipment> equipments = EquipmentsFactory.create();
 
 		/* Create the guards */
 		List<Guard> guards = GuardsFactory.create(equipments);
 
-		/* Create the staff */
-		Staff staff = StaffFactory.create(guards);
-
 		/* Create freemarker configuration */
 		Configuration cfg =  FreemarkerConfigurationFactory.create();
 
 		/* Create the homepage */
-		IndexPageFactory indexPage = new IndexPageFactory(cfg, staff);
+		IndexPageFactory indexPage = new IndexPageFactory(cfg, guards);
 		indexPage.start();
 
 		/* Create a page for each guard */
-		GuardsPageFactory.create(cfg, staff);
+		for (Guard guard : guards) {
+			GuardPageFactory guardPage = new GuardPageFactory(cfg, guard);
+			guardPage.start();
+		}
 
 		/* Create the htpassword file */
-		HtpasswdFactory.create(staff);
+		HtpasswdFactory htpasswd = new HtpasswdFactory(guards);
+		htpasswd.start();
 	}
 
 }
