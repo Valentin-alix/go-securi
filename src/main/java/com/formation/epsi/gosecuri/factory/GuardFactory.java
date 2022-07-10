48c5f6189f54668ad84f90e6d75163dd9f075e2c
valentin alix <valentin-alix.850@outlook.fr>
2022-07-10 13:44:15 +0200

    update properties

--- src/main/java/com/formation/epsi/gosecuri/factory/GuardFactory.java
+++ src/main/java/com/formation/epsi/gosecuri/factory/GuardFactory.java
@@ -1,30 +1,32 @@
 package com.formation.epsi.gosecuri.factory;
 
+import java.util.List;
+
 import com.formation.epsi.gosecuri.model.Equipment;
 import com.formation.epsi.gosecuri.model.Guard;
 
-import java.util.List;
-
 public class GuardFactory {
 
-    /**
-     *
-     * @param shortname Guard’s shortname
-     * @param lastname Guard’s lastname
-     * @param firstname Guard’s firstname
-     * @param job Guard’s job
-     * @param password Guard’s password
-     * @return Guard
-     */
-    public static Guard create(String shortname, String lastname, String firstname, String job, String password, List<Equipment> equipments) {
-        Guard guard = new Guard();
-        guard.setId(shortname);
-        guard.setLastname(lastname);
-        guard.setFirstname(firstname);
-        guard.setJob(job);
-        guard.setPassword(password);
-        guard.setEquipments(equipments);
+	/**
+	 *
+	 * @param shortname Guard’s shortname
+	 * @param lastname  Guard’s lastname
+	 * @param firstname Guard’s firstname
+	 * @param job       Guard’s job
+	 * @param password  Guard’s password
+	 * @return Guard
+	 */
+	public static Guard create(String shortname, String lastname, String firstname, String job, String password,
+			List<Equipment> equipments) {
+		Guard guard = new Guard();
+		guard.setId(shortname);
+		guard.setLastname(lastname);
+		guard.setFirstname(firstname);
+		guard.setJob(job);
+		guard.setPassword(password);
+		guard.setEquipments(equipments);
 
-        return guard;
-    }
+		return guard;
+
+	}
 }
